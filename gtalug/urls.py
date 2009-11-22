fd175d9dad990855b5e9362590a4fe6d64b90252
Myles Braithwaite <me@mylesbraithwaite.com>
2009-11-21 19:44:03 -0500

    Initial commit.

---
+++ gtalug/urls.py
@@ -0,0 +1,20 @@
+from re import escape
+
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'^$',
+		'django.views.generic.simple.direct_to_template',
+		{'template': 'index.html'},
+	),
+	
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+	
+	url(r'^%s/(.*)$' % escape(settings.MEDIA_URL.strip('/')),
+		'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+)
